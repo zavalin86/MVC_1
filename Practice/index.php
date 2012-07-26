29196f3eb090566e195ccdc119b305013097bd34
Andre <zavalin86@gmail.com>
2012-07-26 21:36:57 +0300

    Commit of Practice

---
+++ Practice/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*usual arrays
+ * 
+ * $names = array(
+    'Jessy',
+    'Andrew',
+    'Roman',
+    'Alex',
+    'Tony',
+);
+ *
+ */
+
+
+$names = array(
+  'american' =>  'Jessy',
+  'russian' => 'Андрей',
+  'mexican' =>'Roman',
+  'eglish' => 'Alex',
+  'span' =>'Tom',
+);
+
+echo $names['span'];
+ 
+ 
+if (isset($_REQUEST['check'])) {
+echo $_POST['name'];    
+}
+
+
+
+?>
+
+<form action="index.php?check" method="post" />
+    Name: <input type="text" name="name" /><br />
+    <input type="submit" value="Print name" />
+    
+</form>
